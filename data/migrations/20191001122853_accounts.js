83fe173ff1c522dce82d8d28c6b0b129e24353ba
ch04937 <97hernandez.c@gmail.com>
2019-10-01 13:42:45 -0700

    added email to migrations

--- data/migrations/20191001122853_accounts.js
+++ data/migrations/20191001122853_accounts.js
@@ -3,12 +3,16 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('users', tbl => {
         tbl.increments();
-    
         tbl
-          .string('username', 255)
-          .notNullable()
-          .unique();
-        tbl.string('password', 255).notNullable();
+            .string('username', 255)
+            .notNullable()
+            .unique();
+        tbl
+            .string('email')
+            .notNullable()
+        tbl
+            .string('password', 255)
+            .notNullable();
       })
 };
 
