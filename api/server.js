e38990bc7ae297c923cb1cc55bcd7905bf922143
ch04937 <97hernandez.c@gmail.com>
2019-09-30 23:09:59 -0700

    boiler plate code

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+//tesing server
+server.get('/', (req, res) => {
+    res.send('api is running')
+})
+
+module.exports = server;
