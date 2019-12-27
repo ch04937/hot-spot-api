2f7daf6b039e64a6ab571698ae844095f4c0769f
ch04937 <97hernandez.c@gmail.com>
2019-12-26 22:15:27 -0800

    yelp calls work

--- routes/yelp.js
+++ routes/yelp.js
@@ -1,13 +1,19 @@
 const router = require("express").Router();
+const yelp = require("yelp-fusion");
+const client = yelp.client(process.env.API_KEY, process.env.CLIENT_ID); //yelp key
 
-const Search = require("./yelp-model");
+// const Search = require("./yelp-model");
 
-router.get("/yelp", async (req, res) => {
+router.post("/yelp", async (req, res) => {
 	try {
-		const { location, type } = req.body;
-		const event = await Search.get(location, type);
-
-		res.status(200).json(event);
+		const { location } = req.body;
+		const spot = await client
+			.search({
+				location: location,
+				limit: 2,
+			})
+			.then(res => res.jsonBody.businesses);
+		res.status(200).json(spot);
 	} catch (e) {
 		console.log("error/ cannot get event", e);
 		res.status(500).json({ message: "internal server error" });
